f6ce07e8a0aca956471f3a1c261fadc8fc9d3466
hackycoder0951 <jignesh.vision.17@gmail.com>
2025-09-27 10:27:12 +0530

    Implement 0/1 Knapsack algorithm using Backtracking; add input/output sections for user interaction

--- 07_KnapsackAlgorithmGreedy-DP.py
+++ 07_KnapsackAlgorithmGreedy-DP.py
@@ -2,7 +2,7 @@
 def knapsack_01(weights, values, capacity):
     n = len(weights)
 
-    # DP Table: K[i][w] will store the maximum value 
+    # DP Table: K[i][w] will store the maximum value
     # that can be attained with items 0..i and capacity w
     K = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
 
@@ -14,7 +14,7 @@ def knapsack_01(weights, values, capacity):
             if weights[i - 1] <= w:
                 # Option 1: Include this item -> values[i-1] + value of remaining capacity
                 include = values[i - 1] + K[i - 1][w - weights[i - 1]]
-                
+
                 # Option 2: Exclude this item -> value remains same as previous row
                 exclude = K[i - 1][w]
 
@@ -34,7 +34,8 @@ def fractional_knapsack(weights, values, capacity):
 
     # Compute value-to-weight ratio for each item
     # Store tuple (ratio, weight, value)
-    value_per_weight = [(values[i] / weights[i], weights[i], values[i]) for i in range(n)]
+    value_per_weight = [
+        (values[i] / weights[i], weights[i], values[i]) for i in range(n)]
 
     # Greedy Step: Sort items by decreasing ratio (value/weight)
     value_per_weight.sort(reverse=True)
